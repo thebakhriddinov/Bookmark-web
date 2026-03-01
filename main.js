90bd73a8ba2e88fa0e9c3072701f50c6f51cef4d
Ozodbek <bakhriddinoff01@gmail.com>
2026-03-01 11:59:39 +0500

    add Feature section

--- main.js
+++ main.js
@@ -4,3 +4,27 @@ const menu = document.querySelector(".menu");
 burger.addEventListener("click", () => {
   menu.classList.toggle("active");
 });
+
+const tabs = document.querySelectorAll(".tab");
+const contents = document.querySelectorAll(".content");
+const line = document.querySelector(".line");
+
+function moveLine(el) {
+  line.style.width = el.offsetWidth + "px";
+  line.style.left = el.offsetLeft + "px";
+}
+
+moveLine(document.querySelector(".tab.active"));
+
+tabs.forEach((tab, index) => {
+  tab.addEventListener("click", () => {
+
+    tabs.forEach(t => t.classList.remove("active"));
+    tab.classList.add("active");
+
+    contents.forEach(c => c.classList.remove("active"));
+    contents[index].classList.add("active");
+
+    moveLine(tab);
+  });
+});
